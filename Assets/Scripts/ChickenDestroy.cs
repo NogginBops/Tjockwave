4cc339d82ce227de7d0a73442c8d27d64cf57e28
AlexanderSporny <alexander.sporny@gmail.com>
2017-01-22 12:50:56 +0100

    Food Self-Destroy Script Added

---
+++ Assets/Scripts/ChickenDestroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenDestroy : MonoBehaviour
+{
+
+	public float destroyTimer;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Destroy (gameObject, destroyTimer);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
